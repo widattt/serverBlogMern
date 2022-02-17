f752fa4ab7fd46bd08c3ff508284d1641cd68c39
neydatdev <nguyenvandate@gmail.com>
2022-02-17 18:53:40 +0700

    update

--- server.js
+++ server.js
@@ -8,12 +8,7 @@ const { connectDB } = require('./config/db')
 connectDB()
 const app = express()
 
-app.use(function(req, res, next) {
-    res.header ('Access-Control-Allow-Origin', '*')
-    res.header ('Access-Control-Allow-Credentials', true)
-    res.header ('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS')
-    res.header ('Access-Control-Allow-Headers', 'Content-Type')
-})
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: true, limit: '30mb' }));
 
